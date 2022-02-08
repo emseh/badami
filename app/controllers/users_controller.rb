41cd36a3749e9f1d66cb52f7ca930851e4b4b3ed
andrian-doterb <andrian.rizki@doterb.com>
2022-02-09 00:35:21 +0700

    Update template and update action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,15 +2,20 @@
 
 # app/controllers/users_controller.rb
 class UsersController < ApplicationController
+  before_action :set_user, only: %i[edit update]
   def new
     @user = User.new
   end
 
+  def edit; end
+
   def create
-    @user = User.new(params_user)
+    @user = User.new(user_params)
     respond_to do |format|
       if @user.save
-        format.html { redirect_to articles_path, notice: "Welcome to BADAMI #{@user.username}, You succesfully signed up!" }
+        format.html do
+          redirect_to articles_path, notice: "Welcome to BADAMI #{@user.username}, You succesfully signed up!"
+        end
         format.json { render :show, status: :created, location: @article }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -19,9 +24,25 @@ class UsersController < ApplicationController
     end
   end
 
+  def update
+    respond_to do |format|
+      if @user.update(user_params)
+        format.html { redirect_to articles_path, notice: 'Your account has successfully updated.' }
+        format.json { render :show, status: :ok, location: @user }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
   private
 
-  def params_user
+  def user_params
     params.require(:user).permit(:username, :email, :password)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -12,5 +12,6 @@ class User < ApplicationRecord
                     uniqueness: { case_sensitive: false },
                     length: { maximum: 105 },
                     format: { with: VALID_EMAIL_REGEX }
+  validates :password, presence: true, length: { minimum: 6}
   has_secure_password
 end
