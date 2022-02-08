245d4bd209b5eeb99006b2b53560f32fd4cfa244
andrian-doterb <andrian.rizki@doterb.com>
2022-02-08 17:01:59 +0700

    Add Validation and Association

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,6 +2,7 @@
 
 # app/models/article.rb
 class Article < ApplicationRecord
+  belongs_to :user
   validates :title, presence: true, length: { minimum: 2 }
   validates :body, presence: true, length: { minimum: 6 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,13 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  has_many :articles
+  validates :username, presence: true,
+                       uniqueness: { case_sensitive: false },
+                       length: { minimum: 3, maximum: 25 }
+  VALID_EMAIL_REGEX = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+  validates :email, presence: true,
+                    uniqueness: { case_sensitive: false },
+                    length: { maximum: 105 },
+                    format: { with: VALID_EMAIL_REGEX }
 end
